0406289acadf99ba65487176ef16714c7c7e3754
LucianoMazar <lmazaraojr@gmail.com>
2026-05-22 21:13:39 -0300

    feat(map): add location permition handler

--- lib/views/map_view.dart
+++ lib/views/map_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:mobile_2_bim/widgets/app_shell.dart';
 
 class MapView extends StatefulWidget {
@@ -9,14 +12,77 @@ class MapView extends StatefulWidget {
 }
 
 class _MapViewState extends State<MapView> {
+  final MapController _mapController = MapController();
+  LatLng? _currentLocation;
+  bool _isLoading = true;
+  String _errorMessage = '';
+  bool _mapInitialized = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    try {
+      bool serviceEnabled;
+      LocationPermission permission;
+
+      serviceEnabled = await Geolocator.isLocationServiceEnabled();
+      if (!serviceEnabled) {
+        setState(() {
+          _errorMessage = 'Serviço de localização desabilitado';
+          _isLoading = false;
+        });
+        return;
+      }
+
+      permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          setState(() {
+            _isLoading = false;
+            _errorMessage = 'Permissão de localização negada';
+          });
+          return;
+        }
+      }
+
+      if (permission == LocationPermission.deniedForever) {
+        setState(() {
+          _isLoading = false;
+          _errorMessage = 'Permissão de localização negada permanentemente';
+        });
+        return;
+      }
+
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.best,
+      );
+
+      setState(() {
+        _currentLocation = LatLng(position.latitude, position.longitude);
+        _isLoading = false;
+      });
+
+      if (_mapInitialized) {
+        _mapController.move(_currentLocation!, 16);
+      }
+    } catch (e) {
+      setState(() {
+        _isLoading = false;
+        _errorMessage = 'Erro ao obter localização: $e';
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const AppShell(
-      body: Column(
-        children: [
+    return const AppShell(body: Column(children: [
           
         ],
-      ),
-    );
+      ));
   }
 }
