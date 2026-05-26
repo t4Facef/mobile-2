b59eea83ec0f58f3f91844c39aed6a65a22dd2dd
LucianoMazar <lmazaraojr@gmail.com>
2026-05-26 08:22:27 -0300

    chore(delivery): create delivery page archive

---
+++ lib/views/delivery_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_2_bim/widgets/app_shell.dart';
+
+class DeliveryView extends StatefulWidget {
+  const DeliveryView({super.key});
+
+  @override
+  State<DeliveryView> createState() => _DeliveryViewState();
+}
+
+class _DeliveryViewState extends State<DeliveryView> {
+  @override
+  Widget build(BuildContext context) {
+    return const AppShell(body: Column(children: []));
+  }
+}
