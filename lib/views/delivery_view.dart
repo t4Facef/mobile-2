38096cd107cf30cae3f518e3eb7690828638a5bf
LucianoMazar <lmazaraojr@gmail.com>
2026-05-29 19:57:20 -0300

    feat(delivery): add text area input

--- lib/views/delivery_view.dart
+++ lib/views/delivery_view.dart
@@ -14,10 +14,36 @@ class _DeliveryViewState extends State<DeliveryView> {
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 16),
-      child: const Column(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-        Text("Nova Rota", style: TextStyle(fontSize: 32, fontWeight: FontWeight.w900),),
-      ]),
+          Text(
+            "Nova Rota",
+            style: TextStyle(fontSize: 32, fontWeight: FontWeight.w900),
+          ),
+          SizedBox(height: 12),
+          Text("Organize as paradas e deixei a IA organizar seu tempo"),
+          SizedBox(height: 12),
+          TextField(
+            maxLines: 5,
+            decoration: InputDecoration(
+              hintText:
+                  "Ex: Av. Doutor Hélio Palermo 1200 - Sorvete, para as 20:00\nRua General Osório 150 - Pizza entrega as 21:00...",
+              hintStyle: TextStyle(color: Colors.grey[450] ?? Colors.grey),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+                borderSide: BorderSide(
+                  width: 0.5,
+                  color: themeColors.primary,
+                  style: BorderStyle.none,
+                ),
+              ),
+              
+            ),
+
+          ),
+        ],
+      ),
     );
   }
 }
